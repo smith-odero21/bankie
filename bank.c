4c4d510e7213b9d0ce35fe1b092b7645f559934e
smith-odero21 <joramsodero@gmail.com>
2026-06-21 17:01:24 +0300

    initial commit

--- bank.c
+++ bank.c
@@ -10,8 +10,9 @@ Send ,Recieve and Get a loan( unique loan estmator)
 // Global Variables
 char name[16];
 int age, password;
+int amount = 0;
 
-
+// Create account
 int myAccount(){
     printf("Enter your name: ");
     scanf("%s", name);
@@ -26,14 +27,15 @@ int myAccount(){
 
 }
 
+// login 
 int login() {
     char inputName[16];
     int inputPassword;
 
-    printf("Enter your username: \n");
+    printf("Enter your username: ");
     scanf("%s", inputName);
 
-    printf("Enter your password:\n");
+    printf("Enter your password: ");
     scanf("%d", &inputPassword);
 
     if(strcmp(inputName, name) == 0 && inputPassword == password) {
@@ -41,6 +43,46 @@ int login() {
     } else {
         return 0;
     }
+}
+
+// Deposit
+int depo(int amount) {
+    int dep;
+    printf("Enter your Deposit: ");
+    scanf("%d", &dep);
+    
+    int new_balance = amount + dep;
+    printf("You have successfully Deposited %d\nBalance %d\n\n\tPress Enter to continue....", dep, new_balance);
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+    getchar();
+    printf("\033[H\033[J");
+
+    return new_balance;
+ }
+
+// Withdraw
+int myWithdraw(int amount){
+    int withdraw;
+
+    printf("Enter amount to withdraw: ");
+    scanf("%d", &withdraw);
+
+    if (withdraw > amount){
+        printf("Insufficient funds\n");
+        return amount;
+
+    }
+
+    int new_balance = amount - withdraw;
+    printf("You have successfully withdrawn %d\nBalance %d\n\n\tPress Enter to continue.....\n", withdraw,new_balance);
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+    getchar();
+    printf("\033[H\033[J");
+    
+    return new_balance;
+
 }
 
 
@@ -58,17 +100,51 @@ int main() {
     int loginStatus = 0;
 
     loginStatus = login();
+    
+    
+    int withdrawAmount = 0;
+
 
     if (loginStatus == 1) {
+        printf("Login successful! Welcome, %s.\n\n\tPress Enter to continue......", name);
+        // clear screen 
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF);;
+        getchar();
         printf("\033[H\033[J");
-        printf("Login successful! Welcome, %s.\n", name);
+
+        do{
+            printf("\n\n\t\t=== BANK SYSTEM ===\n1.Deposit\n2.Withdraw\n3.Send\n4.Save\n5.Get a loan\n");
+            int option;
+
+            //deposit = depo(amount);
+            printf("\tBalance %d\n", amount);
+            printf("\tSelect an option: ");
+            scanf("%d", &option);
+
+
+            switch(option) {
+                case 1:
+                    amount = depo(amount);
+                    
+                    break;
+                case 2:
+                    amount = myWithdraw(amount);
+                    break;
+                default:
+                    printf("\n\t\t=== Goodbye===\n\n");
+                    return 0;
+            }
+            
+        } while (loginStatus == 1);
+
     } else {
         printf("\033[H\033[J");
-        printf("Access Denied\n");
+        printf("\n\n\t\t!!!ACCESS DENIED!!!\n\n");
+        return 0;
+        
     }
 
-
-
-
+    
     return 0;
 }
